3cf9ad267acf476839824cbd9d5ff3fa3ad2abd2
Ícaro Castro <icaromscastro@gmail.com>
2018-07-31 16:56:14 -0300

    poll first views

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,18 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    # ex time
     path('time/', views.current_datetime, name='current_datetime'),
-    path('time/plus/2/', views.hours_ahead, name='hours_ahead'),
+    # time plus
+    path('time/plus/<int:hours>/', views.hours_ahead, name='hours_ahead'),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,22 @@
 from django.template.loader import get_template
 from django.template import Context
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 import datetime
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index :)")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 # Create your views here.
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    template = 'polls/detail.html'
+    return render(request, template ,context)
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
@@ -21,10 +27,10 @@ def results(request, question_id):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    return render_to_response('current_datetime.html', {'current_date': now})
+    return render_to_response('polls/current_datetime.html', {'current_date': now})
 
-def hours_ahead(request, offset='2'):
-    offset = int(offset)
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+def hours_ahead(request, hours):
+    #hours = int(hours)
+    dt = datetime.datetime.now() + datetime.timedelta(hours=hours)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (hours, dt)
     return HttpResponse(html)
