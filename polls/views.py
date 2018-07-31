ff00942aace714b963a5a8e7adc43b3e2096995d
Ícaro Castro <icaromscastro@gmail.com>
2018-07-31 19:06:30 -0300

    views update

--- polls/views.py
+++ polls/views.py
@@ -2,15 +2,18 @@ from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import get_object_or_404, render
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 import datetime
 from .models import Question
+from django.db.models import F
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
 # Create your views here.
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -18,16 +21,35 @@ def detail(request, question_id):
     template = 'polls/detail.html'
     return render(request, template ,context)
 
+
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        #Using F() to avoid race condition!!!
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def current_datetime(request):
     now = datetime.datetime.now()
     return render_to_response('polls/current_datetime.html', {'current_date': now})
+
 
 def hours_ahead(request, hours):
     dt = datetime.datetime.now() + datetime.timedelta(hours=hours)
